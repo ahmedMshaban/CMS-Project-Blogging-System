b2b5061c03e9c9867636370679660e2850daffd8
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-12-20 15:36:37 +0300

    Connecting to the DB

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$db["db_server"] = "localhost";
+$db["db_username"] = "root";
+$db["db_password"] = "";
+
+foreach ($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+
+if($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+
+?>
